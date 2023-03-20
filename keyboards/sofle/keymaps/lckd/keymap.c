e609d3fbace7a1a13d859a684e928e0f95894a7c
lucidkodo <lucidkodo@gmail.com>
2023-03-20 13:55:18 +0800

    lckd/sofle: Add underscore

--- keyboards/sofle/keymaps/lckd/keymap.c
+++ keyboards/sofle/keymaps/lckd/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,   KC_F1,     KC_F2,   KC_F3,    KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,   \
   KC_GRV,    KC_1,      KC_2,    KC_3,     KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_F12,   \
   _______,   KC_EXLM,   KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-  _______,   _______,   KC_MINS, _______,  _______, _______, _______,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
+  _______,   _______,   KC_UNDS, KC_MINS,  _______, _______, _______,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
                         _______, _______,  _______, _______, _______,       _______, _______, _______, _______, _______                    \
 ),
 /* RAISE
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, KC_CAPS,                           _______,  _______ , _______,  _______ ,  _______, _______, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           KC_PGUP,  KC_PRVWD, KC_UP  ,  KC_NXTWD,  XXXXXXX, XXXXXXX, \
+  _______, _______, _______, _______, _______, _______,                           KC_PGUP,  KC_PRVWD, KC_UP  ,  KC_NXTWD,  XXXXXXX, XXXXXXX, \
   _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, _______,                           KC_PGDN,  KC_LEFT , KC_DOWN,  KC_RGHT ,  XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,       _______,  KC_WINL,  KC_WINR , XXXXXXX,  XXXXXXX,   XXXXXXX, _______, \
                     _______, _______, _______, _______,  _______,       _______,  _______, _______, _______, _______                         \
